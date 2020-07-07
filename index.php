b57ff228ff867f31ca94e1a9f39b8265f2b8c4e0
444555666ugi <67910165+444555666ugi@users.noreply.github.com>
2020-07-07 17:13:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once("{$base_dir}initcontrols{$ds}index.php");
 ?>
 <div>
 <?php 
-$file = "{$base_dir}initcontrols{$ds}index.php"; 
+$file = "{$base_dir}initcontrols{$ds}home.html"; 
 include_once($file); 
 echo $plHeader;?>   
 </div>
