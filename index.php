c004188c5e58d2a4c92b335fabb87dc55f902ba0
444555666ugi <67910165+444555666ugi@users.noreply.github.com>
2020-07-07 17:08:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-           include_once("home.html")
+$ds = DIRECTORY_SEPARATOR;
+$base_dir = realpath(dirname(__FILE__)  . $ds . '..') . $ds;
+require_once("{$base_dir}initcontrols{$ds}index.php");
 ?>
+<div>
+<?php 
+$file = "{$base_dir}initcontrols{$ds}index.php"; 
+include_once($file); 
+echo $plHeader;?>   
+</div>
